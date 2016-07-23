827f1865a89ec21d1820953a4eeadf07d7f09bdc
lisakawai <lisakawai.jp@gmail.com>
2016-07-23 20:08:14 +0900

    finished

--- cpp/addressbook-impl.cc
+++ cpp/addressbook-impl.cc
@@ -1,7 +1,68 @@
 #include "addressbook-impl.h"
+#include <vector>
 
-using namespace ::std;
+using namespace std;
 
-// To STEP students:
-// Write this file!
-// このファイルの中身を書いてください！
+bool MyAddressBook::Insert(const Contact& contact_new){
+	for (const auto &contact : contacts_){
+		if (contact.email() == contact_new.email() || contact.name() == contact_new.name()){
+			return false;
+		}
+	}
+	contacts_.push_back(contact_new);
+	for (const auto &contact : contacts_){
+		if (contact.email() == contact_new.email() && contact.name() == contact_new.name()){
+			return true;
+		}
+	}
+	return false;
+}
+
+
+const Contact* MyAddressBook::FindEmail(const string &email) const {
+	for (const auto &contact : contacts_){
+		if (contact.email() == email){
+			return &contact;
+		}	
+	}
+	return nullptr;
+}
+
+const Contact* MyAddressBook::FindName(const string &name) const {
+	for (const auto &contact : contacts_){
+		if (contact.name() == name){
+			return &contact;
+		}
+	}
+	return nullptr;
+}
+
+const Contact* MyAddressBook::FindNext(const Contact& contact_cur) const {
+	int index = 0;
+	for (const auto &contact : contacts_){
+		if (contact.name() == contact_cur.name()){
+			if (index + 1 == contacts_.size()){
+				return nullptr;
+			}
+			return &contact + 1;
+		}
+		index++;
+	}
+	return nullptr;
+}
+
+bool MyAddressBook::Delete(const Contact& contact_del) {
+	int index = 0;
+	for (const auto &contact : contacts_){
+		if (contact.name() == contact_del.name()){
+			contacts_.erase(contacts_.begin() + index);
+			return true;
+		}
+		index += 1;
+	}
+	return false;
+}
+
+
+
+
